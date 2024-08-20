cc11924cbfdd85942685f00d60311524d432967b
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-08-20 22:29:12 +0400

    Hiss-info_music

---
+++ assets/js/three.js
@@ -0,0 +1,12 @@
+document.getElementById('playPauseButton').addEventListener('click', function() {
+    var playIcon = this.querySelector('.fa-play');
+    var pauseIcon = this.querySelector('.fa-pause');
+    
+    if (playIcon.style.display === 'none') {
+        playIcon.style.display = 'inline-block';
+        pauseIcon.style.display = 'none';
+    } else {
+        playIcon.style.display = 'none';
+        pauseIcon.style.display = 'inline-block';
+    }
+});
