d0736f70380d3186ceda455f407f8b9ea28ad5c3
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-08-27 02:09:38 +0400

    Hiss-info_music

---
+++ assets/js/klipler.js
@@ -0,0 +1,8 @@
+const video = document.getElementById('myVideo');
+    video.onclick = function() {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    };
