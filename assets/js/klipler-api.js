6efd86c23d342876c6208d4c574dafc15a774b95
Abdullayev Mahammadaga <abdullayevmahammadaga@gmail.com>
2026-01-16 21:15:48 +0400

    ynvr16

--- assets/js/klipler-api.js
+++ assets/js/klipler-api.js
@@ -1,4 +1,5 @@
 const videos = [
+    { title: "Yuxu Yozmaları", src: "../hiss-klips-videos/Hiss  Yuxu Yozmaları.mp4" },
     { title: "XT", src: "../hiss-klips-videos/Hiss  XT.mp4" },
     { title: "Başqa adamlar", src: "../hiss-klips-videos/Hiss  Başqa Adamlar (Visualizer).mp4" },
     { title: "Heç Nə", src: "../hiss-klips-videos/Hiss - Heç Nə.mp4" },
